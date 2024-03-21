ff8ab7b666deef55c7a8f57368e833451a41da90
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-21 12:23:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Starter code
+name = input("Hey, WHat's your name? ") #added input variable for user name
+num1 = 10
+num2 = int(input("Enter a number: ")) 
+#added question inside the input function and changed variables type to interger int()
+
+total1 = num1 + num2
+print(str(num1) + " plus " + str(num2) + " = " + str(total1)) #aded total1 to the line
+
+product = num1 * num2 #added a varibale for multiplcation
+print(str(num1) + " times " + str(num2) + " = " + str(product))
+# Instructions
+
+# Line 4 creates an error. Look carefully to see what is wrong with the code and fix it. done
+
+# Add a text prompt between lines 3 and 4 to tell the user to enter a number. done
+
+# Complete line 8 to concatenate total 1 into the output statement. done
+
+# Add a new variable with a sensible identifier.  Multiply num1 and num2 together 
+# and assign the result into this variable. done
+
+# Output num1, num2 and your new variable in a similar format to line 8. done 
